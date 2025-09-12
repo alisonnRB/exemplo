6e7a35e07f35c3334b70359bbf3923ae38e2b4b2
Charlie Terra <charlieterra.contact@gmail.com>
2025-09-12 20:33:13 -0300

    pronto

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
-print("hello world")
+from drawlib.apis import * #type: ignore
+# função que imprime "Hello World!"
+#print("Hello World!")
 
-for i in range (171):
-    print("hello world")
+# for i in range (171):
+#     print("Hello World!")
+
+config(width=100, height=100)
+
+
+line((10, 10), (90, 90))
+
+circle((25, 75), radius=20)
+
+text((75, 5), "Hello drawlib!")
+
+
+save()
